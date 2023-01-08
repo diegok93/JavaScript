db6933ee085457be0a60917bc6bdc0c3e8077173
diego kleinman <kleinman.diego@gmail.com>
2023-01-08 19:58:03 -0300

    Agrego la pre-entrega 3

---
+++ pre-entrega-3/js/destinos.js
@@ -0,0 +1,47 @@
+const destinos = [
+    {
+        "id": 1,
+        "destino": "Pisa",
+        "img": "../assets/pisa.jpg",
+        "desc1": "Pisa es una ciudad de la región italiana de la Toscana.",
+        "desc2": "Entre los monumentos más importantes figura la catedral, construida en mármol en el siglo XI; y la torre inclinada, del siglo XII, con una altura de 58,36 metros, que sufrió su característica inclinación después de iniciarse su construcción.",
+        "precio": 2500,
+        "promocion": false
+    },
+    {
+        "id": 2,
+        "destino": "Londres",
+        "img": "../assets/londres.jpg",
+        "desc1": "Londres es la capital y mayor ciudad del Reino Unido. Situada a orillas del río Támesis es un importante asentamiento humano desde que fue fundada por los romanos hace casi dos milenios. ",
+        "desc2": "Londres es una ciudad global, uno de los centros neurálgicos en diversos ámbitos.",
+        "precio": 3300,
+        "promocion": false
+    },
+    {
+        "id": 3,
+        "destino": "Madrid",
+        "img": "../assets/madrid.jpg",
+        "desc1": "Madrid es un municipio y una ciudad de España, con categoría histórica de villa, es la capital del Estado y de la Comunidad de Madrid.",
+        "desc2": "El municipio más poblado de España, con 3.305.408 de habitantes, y la segunda ciudad más poblada de la Unión Europea.",
+        "precio": 1600,
+        "promocion": false
+    },
+    {
+        "id": 4,
+        "destino": "Amsterdam",
+        "img": "../assets/amsterdam.jpg",
+        "desc1": "Ámsterdam es la capital de los Países Bajos. La ciudad está situada entre la bahía del IJ, al norte, y a las orillas del río Amstel, al sureste.",
+        "desc2": "Fundada en el siglo XII como un pequeño pueblo pesquero, en la actualidad es la ciudad más grande del país.",
+        "precio": 2800,
+        "promocion": false
+    },
+    {
+        "id": 5,
+        "destino": "Roma",
+        "img": "../assets/roma.jpg",
+        "desc1": "Roma es una ciudad italiana, capital de la región del Lacio y de Italia. Con una población de 2.857.321 habitantes, es el municipio más poblado de Italia.",
+        "desc2": "Es la ciudad con la más alta concentración de bienes históricos y arquitectónicos del mundo.",
+        "precio": 2100,
+        "promocion": false
+    },  
+]
